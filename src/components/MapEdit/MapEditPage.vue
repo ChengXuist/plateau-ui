313bd80254b548c4b86bf1c44f236ca7f76195ab
Cheng Xu <cxu@ipsumtek.com>
2024-10-08 14:26:51 +0200

    feat: zoom and paint coorectly

--- src/components/MapEdit/MapEditPage.vue
+++ src/components/MapEdit/MapEditPage.vue
@@ -16,6 +16,9 @@
     <!-- Obstacle Editor -->
     <div v-if="selectedMode === 'obstacle-editor'" class="mapping-options">
         <div class="drawing-tools mb-3">
+            <button @click="setPanningMode(true)" class="btn btn-outline-secondary me-2">
+                <i class="fas bi-cursor"></i>
+            </button>
             <button @click="setDrawingMode(true)" class="btn btn-outline-secondary me-2">
                 <i class="fas fa-paint-brush"></i>
             </button>
@@ -29,16 +32,10 @@
                 <i class="fas fa-save"></i>
             </button>
         </div>
-
-        <canvas ref="editorCanvas" class="editor-canvas" width="100%" height="auto" @mousedown="beginDrawing" @mousemove="keepDrawing" @mouseup="stopDrawing" @wheel="zoomCanvas"></canvas>
-        <!-- <vue-drawing-canvas ref="VueCanvasDrawing" v-model:image="image" :canvasId="myCanvas" :width="canvasWidth" :height="400" :stroke-type="strokeType" :line-cap="lineCap" :line-join="lineJoin" :fill-shape="fillShape" :eraser="eraser" :lineWidth="line" :color="color" :background-color="backgroundColor" :background-image="backgroundImage" :watermark="watermark" :initial-image="initialImage" saveAs="png" :styles="{
-            border: 'solid 1px #000',
-          }" :lock="disabled" @mousemove="getCoordinate($event)" @wheel="handleScroll" :additional-images="additionalImages" /> -->
+        <canvas ref="editorCanvas" class="editor-canvas" width="100%" height="auto" @mousedown="onMousedown" @mousemove="onMouseMove" @mouseup="onMouseLeave" @wheel="zoomCanvas"></canvas>
     </div>
-    <!-- Path Editor -->
     <div v-else-if="selectedMode === 'path-editor'" class="photo-browser">
         <h2>{{ $t('imageEditor') }}</h2>
-        <!-- Implement image upload and editing here -->
         <input type="file" @change="loadImage" accept="image/*" />
         <canvas ref="imageCanvas" class="image-canvas"></canvas>
     </div>
@@ -46,110 +43,110 @@
 </template>
 
 <script>
-// import VueDrawingCanvas from "vue-drawing-canvas";
-// import config from "@/assets/configuration/map-configuration.json"; // Import configuration
 export default {
-    // components: {
-    //     VueDrawingCanvas,
-    // },
     data() {
         return {
             selectedMode: 'obstacle-editor', // Default mode
             isDrawing: false,
+            isPanning: false,
             canvasWidth: 400,
             backgroundImage: './maps/first_map.png',
             scaleRate: 1.0,
+            draw: true,
 
+            panStartX: 0, // Starting X position of pan
+            panStartY: 0, // Starting Y position of pan
+            panX: 0, // Pan offset X
+            panY: 0, // Pan offset Y
+
+            isMouseDown: false,
+            drawnLines: [],
+            x: null,
+            y: null,
         };
     },
     methods: {
-        setDrawingMode(mode) {
-            this.isDrawing = mode;
+        setPanningMode(mode) {
+            this.isPanning = mode;
+            this.isDrawing = false;
         },
-        // startDrawing(event) {
-        //     this.isDrawing = true;
-        //     // this.draw(event); // Call draw once to start drawing immediately
-        //     this.isDrawing = true; // Start drawing
-        //     this.lastX = event.offsetX; // Get the mouse position
-        //     this.lastY = event.offsetY;
-        // },
-        // stopDrawing() {
-        //     this.isDrawing = false;
-        //     const ctx = this.$refs.editorCanvas.getContext('2d');
-        //     ctx.beginPath(); // Reset the current drawing path
-        // },
-        // handleScroll(event) {
-        //     const delta = event.deltaY; // Positive or negative value
-        //     if (delta > 0) {
-        //         // Scrolling down, decrease canvas width
-        //         if (this.canvasWidth > 100) {
-        //             this.canvasWidth -= 10; // Decrease width by 10px
-        //         }
-        //     } else {
-        //         // Scrolling up, increase canvas width
-        //         this.canvasWidth += 10; // Increase width by 10px
-        //     }
-        //     this.$refs.VueCanvasDrawing.redraw();
-        // },
-        // draw(event) {
-        //     event;
-        //     if (!this.isDrawing) return;
-
-        //     const canvas = this.$refs.editorCanvas;
-        //     const ctx = canvas.getContext('2d');
-        //     const rect = canvas.getBoundingClientRect();
-        //     const x = event.clientX - rect.left;
-        //     const y = event.clientY - rect.top;
-
-        //     console.log("event.clientX: ", event.clientX);
-        //     console.log("event.clientY: ", event.clientY);
-
-        //     ctx.fillStyle = this.drawingMode === 'draw' ? 'black' : 'white'; // Adjust colors as needed
-        //     ctx.strokeStyle = this.drawingMode === 'draw' ? 'black' : 'white';
-        //     ctx.lineWidth = 1; // Adjust line width as needed
-
-        //     ctx.lineTo(x, y);
-        //     ctx.stroke();
-        //     ctx.beginPath();
-        //     ctx.moveTo(x, y);
-
-        //     //         const canvasZoom = canvas.getZoom();
-        //     //         if (!this.isDrawing) return; // If not drawing, do nothing
-
-        //     //  // Get the canvas element using the ref we added earlier
-
-        //     //         console.log("event.clientX: ", (event.clientX - rectLeft)* canvasZoom);
-        //     //         // console.log("rect.offsetLeft: ", rectLeft);
-        //     //         console.log("event.clientY: ", event.clientY - rect.top);
-        //     //         // console.log("rect.top: ", rect.top);
-        //     // // Start drawing a line
-        //     // ctx.beginPath();
-        //     // ctx.moveTo(50, 50);  // Starting point (x, y)
-        //     // ctx.lineTo(100, 100); // Ending point (x, y)
-        //     // ctx.strokeStyle = 'blue'; // Set line color
-        //     // ctx.lineWidth = 2; // Set line width
-        //     // ctx.stroke(); // Draw the line
-        // },
-
+        setDrawingMode(mode) {
+            this.draw = mode;
+            this.isDrawing = mode;
+            this.isPanning = false;
+        },
         getCoordinate(event) {
             let coordinates = this.$refs.VueCanvasDrawing.getCoordinates(event);
             this.x = coordinates.x;
             this.y = coordinates.y;
         },
-
         clearCanvas() {
             const ctx = this.$refs.editorCanvas.getContext('2d');
             ctx.clearRect(0, 0, this.$refs.editorCanvas.width, this.$refs.editorCanvas.height);
         },
+        startPanning(event) {
+            this.isPanning = true;
+            this.panStartX = event.clientX - this.panX; // Track initial mouse position
+            this.panStartY = event.clientY - this.panY;
+        },
+        panMap(event) {
+            if (this.isPanning) {
+                // Update the pan offsets based on mouse movement
+                this.panX = event.clientX - this.panStartX;
+                this.panY = event.clientY - this.panStartY;
+                // Redraw the canvas with the new map position
+                this.loadEditorImage();
+            }
+        },
+        endPanning() {
+            this.isPanning = false;
+        },
+        onMousedown(event) {
+            this.isMouseDown = true;
+            if (this.isDrawing) {
+                this.beginDrawing(event);
+            } else {
+                this.startPanning(event);
+            }
+        },
+        onMouseMove(event) {
+            if (this.isMouseDown) {
+                if (this.isDrawing) {
+                    this.keepDrawing(event)
+                } else {
+                    this.panMap(event);
+                }
+            }
+        },
+        onMouseLeave(event) {
+            if (this.isDrawing) {
+                this.stopDrawing(event)
+            } else {
+                this.endPanning();
+            }
+            this.isMouseDown = false;
+        },
         saveCanvas() {
-
             const canvas = this.$refs.editorCanvas;
-            const link = document.createElement('a');
-            link.href = canvas.toDataURL('image/png'); // Specify the format you want (PNG)
-            link.download = 'drawing.png'; // Default name for the downloaded file
+            const ctx = canvas.getContext('2d');
+            this.panX = 0;
+            this.panY = 0;
+            this.scaleRate = 0;
 
-            // Simulate a click to trigger the download
-            link.click();
+            const img = new Image();
+            img.onload = () => {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+                ctx.drawImage(img, 0, 0); // Draw the image at the center
+                this.redrawLines();
+                const link = document.createElement('a');
+                link.href = canvas.toDataURL('image/png'); // Specify the format you want (PNG)
+                link.download = 'drawing.png'; // Default name for the downloaded file
+                link.click();
+            };
+
+            img.src = './maps/first_map.png'; // Provide the image path here
         },
         loadImage(event) {
             const file = event.target.files[0];
@@ -182,7 +179,10 @@ export default {
 
                 ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
                 ctx.scale(this.scaleRate, this.scaleRate);
+                ctx.translate(this.panX, this.panY);
                 ctx.drawImage(img, 0, 0); // Draw the image at the center
+
+                this.redrawLines();
             };
 
             img.src = './maps/first_map.png'; // Provide the image path here
@@ -191,12 +191,29 @@ export default {
         drawLine(x1, y1, x2, y2) {
             let ctx = this.$refs.editorCanvas.getContext('2d');
             ctx.beginPath();
-            ctx.strokeStyle = this.isDrawing ? 'white' : 'black';
+            ctx.strokeStyle = this.draw ? 'black' : 'white';
             ctx.lineWidth = 2;
-            ctx.moveTo(x1/this.scaleRate, y1/this.scaleRate);
-            ctx.lineTo(x2/this.scaleRate, y2/this.scaleRate);
+            ctx.moveTo((x1 - this.panX) / this.scaleRate, (y1 - this.panY) / this.scaleRate);
+            ctx.lineTo((x2 - this.panX) / this.scaleRate, (y2 - this.panY) / this.scaleRate);
+            //           ctx.moveTo((x1 ) / this.scaleRate, (y1 )/ this.scaleRate);
+            // ctx.lineTo((x2  ) / this.scaleRate, (y2 )/ this.scaleRate);  
             ctx.stroke();
             ctx.closePath();
+        },
+
+        redrawLines() {
+            let ctx = this.$refs.editorCanvas.getContext('2d');
+
+            // Draw all stored lines
+            this.drawnLines.forEach(line => {
+                ctx.beginPath();
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 2;
+                ctx.moveTo(line.start.x, line.start.y);
+                ctx.lineTo(line.end.x, line.end.y);
+                ctx.stroke();
+                ctx.closePath();
+            });
         },
         beginDrawing(e) {
             this.x = e.offsetX;
@@ -206,6 +223,16 @@ export default {
         keepDrawing(e) {
             if (this.isDrawing === true) {
                 this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
+                this.drawnLines.push({
+                    start: {
+                        x: (this.x - this.panX) / this.scaleRate,
+                        y: (this.y - this.panY) / this.scaleRate
+                    },
+                    end: {
+                        x: (e.offsetX - this.panX) / this.scaleRate,
+                        y: (e.offsetY - this.panY) / this.scaleRate
+                    }
+                });
                 this.x = e.offsetX;
                 this.y = e.offsetY;
             }
@@ -213,27 +240,31 @@ export default {
         stopDrawing(e) {
             if (this.isDrawing === true) {
                 this.drawLine(this.x, this.y, e.offsetX, e.offsetY);
-                this.x = 0;
-                this.y = 0;
+                this.drawnLines.push({
+                    start: {
+                        x: (this.x - this.panX) / this.scaleRate,
+                        y: (this.y - this.panY) / this.scaleRate
+                    },
+                    end: {
+                        x: (e.offsetX - this.panX) / this.scaleRate,
+                        y: (e.offsetY - this.panY) / this.scaleRate
+                    }
+                });
                 this.isDrawing = false;
             }
         },
         zoomCanvas(event) {
             event.preventDefault();
 
-            // Adjust the scale (zoom level) based on scroll direction
             const zoomFactor = 0.1;
             if (event.deltaY > 0.5) {
-                // Zoom out
                 this.scaleRate = Math.max(0.5, this.scaleRate - zoomFactor);
             } else {
-                // Zoom in
                 this.scaleRate = Math.min(3, this.scaleRate + zoomFactor);
             }
 
             this.loadEditorImage();
         },
-
     },
     mounted() {
         this.loadEditorImage();
